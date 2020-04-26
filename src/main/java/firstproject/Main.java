87e1eb7cb5de5b451008a25aee3e478486881717
Administrator <Administrator@DESKTOP-MBMOVEH>
2020-04-26 07:22:10 +0530

    Addition of DS

--- src/main/java/firstproject/Main.java
+++ src/main/java/firstproject/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    System.out.println("Hi this is Amit Maurya");
+   System.out.println("You need to complete DS");
 	}
 
 }
